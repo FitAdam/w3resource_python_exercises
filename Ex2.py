b82350b80ca6c2da55c6ead3f2dc28abd11ef3d8
F1tVdvm <adam.george617@gmail.com>
2020-05-15 14:50:18 +0100

    Second day

---
+++ Ex2.py
@@ -0,0 +1,14 @@
+""" 2. Write a Python program to get the Python version you are using.  """
+
+import sys
+"""This module provides access to some variables used or
+ maintained by the interpreter and 
+ to functions that interact strongly with the interpreter. 
+It is always available. """
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+print(sys.copyright)
+print(sys.platform)
