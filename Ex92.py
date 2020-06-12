267f2123ead83549434e32c8016663413c3cb625
Fit Adam <adam.george617@gmail.com>
2020-06-12 23:03:21 +0100

    again  :cop:

---
+++ Ex92.py
@@ -0,0 +1,7 @@
+"""92. Write a Python program to define
+ a string containing special characters in various forms."""
+
+def define(some_str):
+    return print(fr"""{some_str}""")
+
+define('jf;asfprint("afjsdlj")f;a@@@@~~~')
