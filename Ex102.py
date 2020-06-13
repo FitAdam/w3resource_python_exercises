3cdbb94118236d756fa5789ff75f58cccfe7687a
Fit Adam <adam.george617@gmail.com>
2020-06-13 14:59:43 +0100

    subprocess

--- Ex102.py
+++ Ex102.py
@@ -1,2 +1,10 @@
 """102. Write a Python program to get system command output."""
+import subprocess
 
+
+def get_syscmd_output(command):
+    returned_text = subprocess.check_output(command, shell=True, universal_newlines=True)
+    return returned_text
+
+
+print(get_syscmd_output('dir'))
