0e47a850941bfef9c878d5e460a4747846640e70
FitAdam <adam.george617@gmail.com>
2020-06-27 13:58:35 +0200

    :done:

--- Ex149.py
+++ Ex149.py
@@ -2,4 +2,17 @@
  a positive integer and returns the sum of the cube of all the positive 
  integers smaller than the specified number."""
 
- 
+def foo(x):
+ newrange = list(range(0,x))
+ sum = 0
+ cube_range = []
+
+ for element in newrange:
+ 	cube_range.append(element * element * element)
+
+ for element in cube_range:
+ 	sum+=element
+
+ return sum
+
+print(foo(8))
