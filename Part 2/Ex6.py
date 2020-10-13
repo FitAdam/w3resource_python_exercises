e2312bb8f104114b32596374a2d96c6d387565e0
FitAdam <adam.tutak@protonmail.com>
2020-10-13 01:17:04 +0100

    starting 6th

---
+++ Part 2/Ex6.py
@@ -0,0 +1,16 @@
+"""
+6. Write a Python program to print a long text, convert the string to a list and print all the words and their frequencies. 
+"""
+
+def analyse(text):
+    lst = list(text)
+    print(lst)
+
+
+text = """ C#
+C# (pronounced "C sharp") is a simple, modern, object-oriented, and type-safe programming language.
+
+Its roots in the C family of languages makes C# immediately familiar to C, C++, Java, and JavaScript programmers."""
+
+analyse(text)
+# TODO
